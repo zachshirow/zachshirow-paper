b7258e4939e6742e32d2e549d1ac1d16adbe7745
Zachary Shirmohammadli <zachshirow@gmail.com>
2024-11-29 22:09:32 +0330

    updated the time read function to farsi

--- src/utils/remark-reading-time.mjs
+++ src/utils/remark-reading-time.mjs
@@ -5,8 +5,12 @@ export function remarkReadingTime() {
   return function (tree, { data }) {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
+    const readingTimeMsg = Math.ceil(readingTime.minutes) + " دقیقه مطالعه"
     // readingTime.text will give us minutes read as a friendly string,
     // i.e. "3 min read"
-    data.astro.frontmatter.minutesRead = readingTime.text;
+
+
+
+    data.astro.frontmatter.minutesRead = readingTimeMsg;
   };
 }
